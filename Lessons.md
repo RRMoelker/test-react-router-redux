c31cbec840d43a88cee0493a1667fd024829cbbe
RM <RRMoelker@users.noreply.github.com>
2018-09-07 12:24:33 +0200

    lessons updated

--- Lessons.md
+++ Lessons.md
@@ -1,1 +1,40 @@
-* Get parameters aren't a common thing to match in route, might be impossible with Vanilla router
+
+## Query params routing 
+Get parameters aren't a common thing to match in route, might be impossible with Vanilla router.
+Best option is probally to parse them ourselves: https://tylermcginnis.com/react-router-query-strings/
+
+## Reverse URL construction with query string
+Doesn't seem to be a way with the query string support lacking.
+
+We'll have to create the query string ourselves....
+
+## Programmatic query string navigation
+
+Best option is probably: 
+props.history.push({
+  pathname: '',
+  search: '?lat=2lon=42'
+});
+
+<b>We are responsible for keeping STATE and URL in sync... Not handled by libraries.</b>
+
+Say state:
+
+```
+{
+  map: {
+    lat: 1,
+    lng: 2
+  },
+  router: ...
+}
+```
+
+with URL:
+
+```
+/map/?lat=1&lng=2
+```
+
+if we pan, we need to do `navigation` through either Redux router or React router 
+AND we need to update the `state.map.lat` and `state.map.lng`.
--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
+import {push} from 'connected-react-router'
+
 
 const Page2 = (props) => {
   const params = props.match.params;
-  const { pathname, search, hash } = props;
-  // console.log(params);
+  const { doNavigate, pathname, search, hash} = props;
+
+  // const doNavigateHistory = () => props.history.push('?lat=2lon=42');
+  const doNavigateHistory = () => props.history.push({
+    pathname: '',
+    search: '?lat=2lon=42'
+  });
+
+
   return (
     <div>
       <h1>Page2</h1>
+      <button onClick={doNavigate}>Navigate through store.dispatch</button>
+      <button onClick={doNavigateHistory}>Navigate using history</button>
+
       <div>
         <h2>Redux router params</h2>
         <pre>{JSON.stringify(params, null, 2)}</pre>
@@ -33,4 +45,10 @@ const mapStateToProps = state => ({
   hash: state.router.location.hash,
 });
 
-export default connect(mapStateToProps)(Page2)
+const mapDispatchToProps = dispatch => ({
+  doNavigate: () => dispatch(push({
+    query: 'foo'
+  }))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page2)
